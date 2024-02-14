014031bce9838e59808551510549b3379c1aa37e
ww_side <daniil.metelia@gmail.com>
2024-02-14 16:52:06 +0200

    feat(client): add types

---
+++ client/src/types/quote.ts
@@ -0,0 +1,10 @@
+export type QuoteType = {
+  ticker: string;
+  exchange: string;
+  price: string;
+  change: string;
+  change_percent: string;
+  dividend: string;
+  yield: string;
+  last_trade_time: Date;
+};
