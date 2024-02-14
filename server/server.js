7902aedaa841598fb001ca6d31036aab8f5a109d
ww_side <daniil.metelia@gmail.com>
2024-02-14 16:37:55 +0200

    refactor(server): add real real change and change percent

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-'use strict';
-const express = require('express');
-const http = require('http');
-const io = require('socket.io');
-const cors = require('cors');
+"use strict";
+const express = require("express");
+const http = require("http");
+const io = require("socket.io");
+const cors = require("cors");
 
 const FETCH_INTERVAL = 5000;
 const PORT = process.env.PORT || 4000;
 
 const tickers = [
-  'AAPL', // Apple
-  'GOOGL', // Alphabet
-  'MSFT', // Microsoft
-  'AMZN', // Amazon
-  'FB', // Facebook
-  'TSLA', // Tesla
+  "AAPL", // Apple
+  "GOOGL", // Alphabet
+  "MSFT", // Microsoft
+  "AMZN", // Amazon
+  "FB", // Facebook
+  "TSLA", // Tesla
 ];
 
 function randomValue(min = 0, max = 1, precision = 0) {
@@ -23,23 +23,50 @@ function randomValue(min = 0, max = 1, precision = 0) {
 
 function utcDate() {
   const now = new Date();
-  return new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+  return new Date(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds()
+  );
 }
 
 function getQuotes(socket) {
+  const previousQuotes = socket.previousQuotes || {};
 
-  const quotes = tickers.map(ticker => ({
-    ticker,
-    exchange: 'NASDAQ',
-    price: randomValue(100, 300, 2),
-    change: randomValue(0, 200, 2),
-    change_percent: randomValue(0, 1, 2),
-    dividend: randomValue(0, 1, 2),
-    yield: randomValue(0, 2, 2),
-    last_trade_time: utcDate(),
-  }));
+  const quotes = tickers.map((ticker) => {
+    const previousPrice = previousQuotes[ticker]
+      ? previousQuotes[ticker].price
+      : null;
+    const price = randomValue(100, 300, 2);
+    const change =
+      previousPrice !== null
+        ? (price - previousPrice).toFixed(2)
+        : randomValue(0, 200, 2);
+    const changePercent =
+      previousPrice !== null
+        ? ((change / previousPrice) * 100).toFixed(2)
+        : randomValue(0, 1, 2);
 
-  socket.emit('ticker', quotes);
+    const quote = {
+      ticker,
+      exchange: "NASDAQ",
+      price,
+      change,
+      change_percent: changePercent,
+      dividend: randomValue(0, 1, 2),
+      yield: randomValue(0, 2, 2),
+      last_trade_time: utcDate(),
+    };
+
+    previousQuotes[ticker] = quote;
+    return quote;
+  });
+
+  socket.emit("ticker", quotes);
+  socket.previousQuotes = previousQuotes;
 }
 
 function trackTickers(socket) {
@@ -47,11 +74,11 @@ function trackTickers(socket) {
   getQuotes(socket);
 
   // every N seconds
-  const timer = setInterval(function() {
+  const timer = setInterval(function () {
     getQuotes(socket);
   }, FETCH_INTERVAL);
 
-  socket.on('disconnect', function() {
+  socket.on("disconnect", function () {
     clearInterval(timer);
   });
 }
@@ -63,15 +90,15 @@ const server = http.createServer(app);
 const socketServer = io(server, {
   cors: {
     origin: "*",
-  }
+  },
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-socketServer.on('connection', (socket) => {
-  socket.on('start', () => {
+socketServer.on("connection", (socket) => {
+  socket.on("start", () => {
     trackTickers(socket);
   });
 });
