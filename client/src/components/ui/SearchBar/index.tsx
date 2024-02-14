1cd6888589669f155ed13e0ba073dd672cfb7fdb
ww_side <daniil.metelia@gmail.com>
2024-02-14 16:42:31 +0200

    feat(client): add search bar

---
+++ client/src/components/ui/SearchBar/index.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent, type FC } from "react";
+import { Box, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import st from "./styles.module.css";
+
+interface SearchBarProps {
+  onSearchChange: (query: string) => void;
+}
+
+const SearchBar: FC<SearchBarProps> = ({ onSearchChange }) => {
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onSearchChange(event.target.value);
+  };
+
+  return (
+    <Box className={st.searchBarWrapper}>
+      <SearchIcon className={st.searchIcon} />
+      <TextField
+        label="Search tickers"
+        variant="standard"
+        onChange={handleSearchChange}
+      />
+    </Box>
+  );
+};
+
+export default SearchBar;
