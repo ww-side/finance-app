640d710679bd59a2eb8ceaf194a9ff472e1c61c1
ww_side <daniil.metelia@gmail.com>
2024-02-14 16:50:59 +0200

    feat(client): add pages

---
+++ client/src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import QuotesList from "@/components/common/QuotesList";
+
+const HomePage = () => {
+  return (
+    <main>
+      <QuotesList />
+    </main>
+  );
+};
+
+export default HomePage;
---
+++ client/src/pages/NotFoundPage/index.tsx
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {
+  return <main>Not found</main>;
+};
+
+export default NotFoundPage;
