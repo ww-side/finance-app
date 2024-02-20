1c8570d4a011098f306d5413d881b4974c52511a
ww_side <daniil.metelia@gmail.com>
2024-02-20 20:54:34 +0200

    style(client): add margin top in root layout

--- client/src/components/layouts/RootLayout/index.tsx
+++ client/src/components/layouts/RootLayout/index.tsx
@@ -1,4 +1,5 @@
 import { type FC, type ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
 import { Box } from "@mui/material";
 import Header from "@/components/common/Header";
 import st from "./style.module.css";
@@ -8,6 +9,7 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
     <>
       <Header />
       <Box className={st.rootLayout}>{children}</Box>
+      <Toaster position="bottom-left" reverseOrder={false} />
     </>
   );
 };
